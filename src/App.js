1c904a931200d238f90b2eaddeaa828e359eda75
Jamie Rushford <jar.rushford@gmail.com>
2018-10-23 16:42:31 -0600

    Import Data, Give Initial Props to Search, Audio components

--- src/App.js
+++ src/App.js
@@ -4,15 +4,34 @@ import Header from './Header';
 import Search from './Search';
 import ConcertDisplay from './ConcertDisplay';
 import AudioPlayer from './AudioPlayer';
+import showData from './data/data.js';
+import setlistData from './data/data.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSong: null,
+      currentSearch: null,
+      concertData: showData,
+      setlistData: setlistData 
+    }
+  }
+
+  updateCurrentSong = (searchValue) => {
+    this.setState({
+      currentSearch: searchValue
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <Search />
-        <ConcertDisplay />
-        <AudioPlayer />
+        <Search updateCurrentSong={this.updateCurrentSong}/>
+        <ConcertDisplay concertData={this.state.concertData}
+                        setlistData={this.state.setlistData}/>
+        <AudioPlayer currentSong={this.state.currentSong}/>
       </div>
     );
   }
