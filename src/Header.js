eddf2f9668ec8224b17e1a38fdf51d9a7a7afec3
Loryn Mason <masonloryn@gmail.com>
2018-10-24 17:15:03 -0600

    Styled header and search bar

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,9 @@ export default function Header() {
 
   return (
     <header>
-      <h1 className="header-plenty">Plenty</h1>
-      <h1 className="header-of">of</h1>
-      <img src="https://www.logolynx.com/images/logolynx/a4/a48597184b96ba3cb64ee133bcd52ad4.png"/>
+      <h1 className="header-plenty">PLENTY</h1>
+      <h1 className="header-of">OF</h1>
+      <img className="header-img" src="https://www.logolynx.com/images/logolynx/a4/a48597184b96ba3cb64ee133bcd52ad4.png"/>
     </header>
   )
 
