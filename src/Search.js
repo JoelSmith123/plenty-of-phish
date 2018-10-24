e56de323849ad0e122ab747c6d38c430628c0070
Loryn Mason <masonloryn@gmail.com>
2018-10-24 12:40:52 -0600

    Connected search bar to parent

--- src/Search.js
+++ src/Search.js
@@ -4,13 +4,28 @@ import './Search.css';
 export default class Search extends Component {
   constructor() {
     super();
+    this.state = {
+      phishingFor: ''
+    }
   }
 
   render() {
     return (
       <form>
         <button>View All</button>
-        <input type="text" placeholder="Search for a Song, Venue, or Show Date" />
+        <input 
+          type="text"
+          placeholder="Search for a Song, Venue, or Show Date"
+          value={this.state.phishingFor} 
+          onChange={ (e) => {
+            this.setState({
+              phishingFor: e.target.value
+            })
+            e.preventDefault();
+            this.props.updateCurrentSong(this.state.phishingFor)
+            console.log(this.state.phishingFor) 
+          }} 
+        />
         <button>Random Shows</button>
       </form>
     )
