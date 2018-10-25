4de5e8673538b98b5e5cfed10be21a79c0661e7b
Joel Smith <joelwsmith123@gmail.com>
2018-10-24 21:13:25 -0600

    Add song index submission to App.js state on song click

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
       currentShow: {venue: {name: "Madison Square Garden", location: "New York, NY"}},
       currentSearch: null,
       concertData: [],
-      setlistData: {}
+      setlistData: {1903: ['nothing': 'void', 'nothing': 'void', 'nothing': 'void']}
     }
   }
 
@@ -46,7 +46,15 @@ class App extends Component {
     this.setState({
       currentSearch: searchValue
     });
-    console.log(this.state.currentSearch)
+    console.log(this.currentSearch)
+    // console.log(this.state.currentSearch)
+  }
+
+  updateCurrentSongIndex = (songIndex) => {
+    this.setState({
+      currentSong: songIndex
+    });
+    console.log(this.state.currentSong)
   }
 
   render() {
@@ -56,7 +64,8 @@ class App extends Component {
         <Search updateCurrentSong={this.updateCurrentSong}/>
         <ConcertDisplay concertData={this.state.concertData}
                         setlistData={this.state.setlistData}
-                        updateCurrentSong={this.updateCurrentSong}/>
+                        updateCurrentSong={this.updateCurrentSong}
+                        updateCurrentSongIndex={this.updateCurrentSongIndex}/>
         <AudioPlayer currentSong={this.state.currentSong}
                       currentSetlist={this.state.currentSetlist}
                       currentShow={this.state.currentShow}
--- src/ExtendedView.js
+++ src/ExtendedView.js
@@ -9,17 +9,29 @@ export default class ExtendedView extends Component {
     }
   }
 
+  convertTime = (ms) => {
+    var milliseconds = parseInt(ms);
+    var hours = Math.floor(milliseconds / 3600000);
+    var minutes = Math.floor((milliseconds - (hours * 3600000)) / 60000);
+    var seconds = parseInt((milliseconds - (hours * 3600000) - (minutes * 60000)) / 1000);
+    if (seconds < 10) {
+      return `${minutes}:0${seconds}`
+    }
+    return `${minutes}:${seconds}`
+  }
+
   render() {
     return (
       <table className='song-table'>
         <tbody>
             {
-              this.props.setlist.map((song, index) => {
+              this.props.setlist[1903].map((song, index) => {
                 return <tr className='song-table-row' onMouseEnter={(e) => this.setState({hover: true})} onMouseLeave={(e) => this.setState({hover: false})} key={index}>
                           <td className={ this.state.hover ? 'far fa-play-circle' : 'display-mode-none' }></td>
                           <td className={ this.state.hover ? 'display-mode-none' : 'song-table-position' }>{song.position}</td>
-                          <td className='song-table-title' onClick={(e) => this.props.updateCurrentSong(song)}>{song.title}</td>
-                        </tr>
+                          <td className='song-table-title' onClick={(e) => this.props.updateCurrentSongIndex(index)}>{song.title}</td>
+                          <td className='song-table-duration' >{this.convertTime(song.duration)}</td>
+                        </tr> 
               })                            
             }
         </tbody>
