83f77766833e4bc4c98ebc3bd26097dd78871e1b
Joel Smith <joelwsmith123@gmail.com>
2018-10-24 21:41:20 -0600

    Fix ExtendedView styling

--- src/ExtendedView.js
+++ src/ExtendedView.js
@@ -4,9 +4,6 @@ import './ExtendedView.css';
 export default class ExtendedView extends Component {
   constructor() {
     super()
-    this.state = {
-      hover: false
-    }
   }
 
   convertTime = (ms) => {
@@ -20,15 +17,15 @@ export default class ExtendedView extends Component {
     return `${minutes}:${seconds}`
   }
 
+
   render() {
     return (
       <table className='song-table'>
         <tbody>
             {
               this.props.setlist[1903].map((song, index) => {
-                return <tr className='song-table-row' onMouseEnter={(e) => this.setState({hover: true})} onMouseLeave={(e) => this.setState({hover: false})} key={index}>
-                          <td className={ this.state.hover ? 'far fa-play-circle' : 'display-mode-none' }></td>
-                          <td className={ this.state.hover ? 'display-mode-none' : 'song-table-position' }>{song.position}</td>
+                return <tr className='song-table-row' key={index}>
+                          <td className='song-table-position'>{song.position}</td>
                           <td className='song-table-title' onClick={(e) => this.props.updateCurrentSongIndex(index)}>{song.title}</td>
                           <td className='song-table-duration' >{this.convertTime(song.duration)}</td>
                         </tr> 
