4e4c39da240db838d20a70c5fe89ab11a2226cde
Jamie Rushford <jar.rushford@gmail.com>
2018-10-31 15:04:54 -0600

    Updates

---
+++ precache-manifest.ab8a0b2faf746a64e958ba73267afa2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1da343506b5fd5e42339",
+    "url": "/plenty-of-phish/static/js/runtime~main.1da34350.js"
+  },
+  {
+    "revision": "d00050e4206fd93dde9d",
+    "url": "/plenty-of-phish/static/js/main.d00050e4.chunk.js"
+  },
+  {
+    "revision": "91c61df60f232905cc4b",
+    "url": "/plenty-of-phish/static/js/1.91c61df6.chunk.js"
+  },
+  {
+    "revision": "d00050e4206fd93dde9d",
+    "url": "/plenty-of-phish/static/css/main.cdcc317d.chunk.css"
+  },
+  {
+    "revision": "40df86981489e8c12dcd22fa1809d3c3",
+    "url": "/plenty-of-phish/index.html"
+  }
+];
